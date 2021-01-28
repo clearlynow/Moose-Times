72e1440e7b1ae3fafcb7a35a8578cb31b8f6f623
clearlynow <59779189+clearlynow@users.noreply.github.com>
2021-01-27 19:36:55 -0700

    Initial Commit

---
+++ MultiplicationGame/MultiplicationGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  MultiplicationGameApp.swift
+//  MultiplicationGame
+//
+//  Created by Alison Gorman on 1/27/21.
+//
+
+import SwiftUI
+
+@main
+struct MultiplicationGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
